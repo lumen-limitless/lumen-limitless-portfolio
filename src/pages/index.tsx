1094249962e4b22727c6a2bf818b92c47b3c19e7
lumen-limitless <lumenlimitless@gmail.com>
2022-12-28 12:43:32 -0500

    switched to pnpm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -220,7 +220,6 @@ export default function Home() {
           <div className="mt-6 flex gap-6">
             <SocialLink
               href="https://twitter.com/LumenLimitless"
-              x
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
